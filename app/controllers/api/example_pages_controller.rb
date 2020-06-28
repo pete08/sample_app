a9febd0e75d4a05bd6d5989ce2e139cc4f814f1c
PeterH <pete08@gmail.com>
2020-06-28 11:30:16 -0500

    Add controller actions to use jb file views instaed of rendering within controllers

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,2 +1,24 @@
 class Api::ExamplePagesController < ApplicationController
+  def hello
+    render json: {message: "hello there", time:Time.new}
+  end
+  def time
+    @time_message = Time.new
+    @message = "bob there there"
+    render 'practice.json.jb'
+  end
+  def whatup
+    @message = "what's up"
+    render 'whatsup.json.jb'
+  end
+  def goodbye
+    render json: {message: "Goodbye, and thanks for all the fish."}
+  end
+  def list_of_foods
+    render json: [["kiwi", "crossiont", "eggs", "applejacks", "pizza"], ["elephant ear", "icecream","conali"]]
+  end
+  def practice
+    render "the_brian.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,23 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+namespace :api do
+  get "/hi" => "example_pages#hello"
+end
+namespace :api do
+  get "/goodbye" => "example_pages#goodbye"
+end
+namespace :api do
+  get "/whatsup" => "example_pages#whatup"
+end
+namespace :api do
+  get "/time" => "example_pages#time"
+end
+
+
+
+namespace :api do
+  get "/foods" => "example_pages#list_of_foods"
+end
+
 end
